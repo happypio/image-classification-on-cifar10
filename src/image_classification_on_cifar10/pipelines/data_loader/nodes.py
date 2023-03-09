532ecf2fb30073ca8772ae501c6eae99a8ed4ca4
Rafał Nowak <rafal.nowak@tooploox.com>
2023-03-09 10:57:50 +0100

    refactor params

--- src/image_classification_on_cifar10/pipelines/data_loader/nodes.py
+++ src/image_classification_on_cifar10/pipelines/data_loader/nodes.py
@@ -4,30 +4,47 @@ generated using Kedro 0.18.6
 """
 import torchvision
 import ssl
-ssl._create_default_https_context = ssl._create_unverified_context
 
-# standard cast into Tensor and pixel values normalization in [-1, 1] range
-transform = torchvision.transforms.Compose([
-    torchvision.transforms.ToTensor(),
-    torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-])
+from icecream import ic
 
-# extra transfrom for the training data, in order to achieve better performance
-train_transform = torchvision.transforms.Compose([
-    torchvision.transforms.ToTensor(),
-    torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-    torchvision.transforms.RandomCrop(32, padding=4, padding_mode='reflect'), 
-    torchvision.transforms.RandomHorizontalFlip(), 
-])
+# ssl._create_default_https_context = ssl._create_unverified_context  # FIXME: ???
 
-def download_train_data(train: bool)-> torchvision.datasets.cifar.CIFAR10:
-    trainset = torchvision.datasets.CIFAR10(
-        root='./data/01_raw/', train=train, download=True, transform=train_transform
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def load_transforms(train_transform_params: dict[str]):
+    # TODO:
+    if "train" in train_transform_params:
+        return torchvision.transforms.Compose(
+            [
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                torchvision.transforms.RandomCrop(
+                    32, padding=4, padding_mode="reflect"
+                ),
+                torchvision.transforms.RandomHorizontalFlip(),
+            ]
+        )
+    else:
+        return torchvision.transforms.Compose(
+            [
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ]
+        )
+
+
+def download_data(
+    train: bool, transform_params: dict
+) -> torchvision.datasets.cifar.CIFAR10:
+
+    logger.debug(transform_params)
+
+    return torchvision.datasets.CIFAR10(
+        root="./data/01_raw/",
+        train=train,
+        download=True,
+        transform=load_transforms(transform_params),
     )
-    return trainset
-
-def download_test_data(train: bool) -> torchvision.datasets.cifar.CIFAR10:
-    testset = torchvision.datasets.CIFAR10(
-        root='./data/01_raw/', train=train, download=True, transform=transform
-    )
-    return testset
