40648abc5e7dca8f0d26219f2e2ab1a3b9563828
Piotrek <piterpiesiak@gmail.com>
2023-04-29 17:43:54 +0200

    Convert to float params

--- src/image_classification_with_vit/pipelines/download_data/nodes.py
+++ src/image_classification_with_vit/pipelines/download_data/nodes.py
@@ -3,7 +3,7 @@ from datasets import load_dataset
 #load dataset
 def load_cifar10(train_size, test_size, val_prct):    
     train_ds, test_ds = load_dataset('cifar10', split=[f"train[:{train_size}]", f"test[:{test_size}]"])
-    splits = train_ds.train_test_split(test_size=val_prct)
+    splits = train_ds.train_test_split(test_size=float(val_prct))
     train_ds = splits['train']
     val_ds = splits['test']
     return train_ds, val_ds, test_ds
